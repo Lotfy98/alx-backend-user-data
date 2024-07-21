79ef57b2e7055554e215e29d6c64f38098bb8e34
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-07-22 01:49:11 +0300

    0x01. Basic authentication

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,12 +14,6 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
-
-
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -32,3 +26,9 @@ def unauthorized(error) -> str:
     """Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
