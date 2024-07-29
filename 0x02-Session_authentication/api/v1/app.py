8b60608c80cd0d91a75200bc765e6fe1a45dc74c
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-07-30 02:41:55 +0300

    0x02. Session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -64,7 +64,8 @@ def authenticate_user():
             "/api/v1/auth_session/login/",
         ]
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             user = auth.current_user(request)
             if user is None:
